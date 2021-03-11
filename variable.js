0bddb0c472fba5458ce96192c8ff4179dbaf3f7d
ericabyss <erica051746@naver.com>
2021-03-11 23:56:16 +0900

    start js

---
+++ variable.js
@@ -0,0 +1,12 @@
+//  1. Use Strict
+// added in ES 5
+// use this for Vanila Javascript
+'use strict';
+
+// 2. Variable
+// let (added in ES 6)
+
+let name = 'erica'
+console.log(name);
+name = 'hello';
+console.log(name);
