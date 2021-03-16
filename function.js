1769265870abe4d4fe9a1b8d510e88ef5fc0cfae
ericabyss <erica051746@naver.com>
2021-03-16 17:45:52 +0900

    5. function_1

---
+++ function.js
@@ -0,0 +1,12 @@
+// Function
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... retuen; }
+// one function === one thing
+// naming: doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
