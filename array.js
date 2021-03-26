cbd0d4a8d4ec5d59b669406770d8b37a0a11ef00
ericabyss <erica051746@naver.com>
2021-03-27 02:18:53 +0900

    array 끝

--- array.js
+++ array.js
@@ -3,18 +3,19 @@
 // Array🎉
 
 // 1. Declaration
-const arr1 = new Array();
-const arr2 = [1, 2];
+const arr1 = new Array(); // 방법1 : new 키워드
+const arr2 = [1, 2]; // 방법 2 0[], 1[]
 
 // 2. Index position
 const fruits = ['🍎', '🍌'];
-console.log(fruits);
-console.log(fruits.length);
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-console.log(fruits[fruits.length - 1]);
+console.log(fruits); // ['🍎', '🍌']
+console.log(fruits.length); // 2
+console.log(fruits[0]); // object->fruits['name'], name에 상응하는 value가 나옴 🍎
+console.log(fruits[1]); //🍌
+console.log(fruits[2]); //undefined
+console.log(fruits[fruits.length - 1]); //마지막 인덱스 접근, 0부터 시작하기 때문
 console.clear();
+
 // 3. Looping over an array
 // print all fruits
 // a. for
@@ -27,7 +28,10 @@ for (let fruit of fruits) {
   console.log(fruit);
 }
 
-// c. forEach
+// c. forEach <- callback 함수
+/* fruits.forEach(function (fruit, index, array){
+    console.log(fruits, index, array);
+}); 익명함수 => arrow함수*/
 fruits.forEach((fruit) => console.log(fruit));
 
 // 4. Addtion, deletion, copy
@@ -40,27 +44,27 @@ const poped = fruits.pop();
 fruits.pop();
 console.log(fruits);
 
-// unshift: add an item to the benigging
+// unshift: add an item to the benigging 앞에 들어가도록
 fruits.unshift('🍓', '🍋');
 console.log(fruits);
 
-// shift: remove an item from the benigging
+// shift: remove an item from the benigging 앞부터 빠지도록, 당겨오는
 fruits.shift();
 fruits.shift();
 console.log(fruits);
 
 // note!! shift, unshift are slower than pop, push
-// splice: remove an item by index position
+// splice: remove an item by index position 지정위치에서 데이터 삭제 어디서부터..
 fruits.push('🍓', '🍑', '🍋');
 console.log(fruits);
-fruits.splice(1, 1);
+fruits.splice(1, 1); //원하는 인덱스, 몇개
 console.log(fruits);
-fruits.splice(1, 0, '🍏', '🍉');
+fruits.splice(1, 0, '🍏', '🍉');// 지워진 자리에 들어감
 console.log(fruits);
 
-// combine two arrays
+// combine two arrays concat 연결, 새로운 배열 리턴
 const fruits2 = ['🍐', '🥥'];
-const newFruits = fruits.concat(fruits2);
+const newFruits = fruits.concat(fruits2); //
 console.log(newFruits);
 
 // 5. Searching
@@ -69,15 +73,15 @@ console.clear();
 console.log(fruits);
 console.log(fruits.indexOf('🍎'));
 console.log(fruits.indexOf('🍉'));
-console.log(fruits.indexOf('🥥'));
+console.log(fruits.indexOf('🥥')); //-1
 
-// includes
+// includes 있는지 없는지 T,F
 console.log(fruits.includes('🍉'));
-console.log(fruits.includes('🥥'));
+console.log(fruits.includes('🥥')); //false
 
 // lastIndexOf
 console.clear();
 fruits.push('🍎');
 console.log(fruits);
 console.log(fruits.indexOf('🍎'));
-console.log(fruits.lastIndexOf('🥥'));
+console.log(fruits.lastIndexOf('🥥')); //마지막 인덱스 추가
